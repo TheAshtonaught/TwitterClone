1af2ac96bfc44abc162c8c1867543f48f0fa1e96
Ashton Morgan <ashtomorgan3@gmail.com>
2017-03-26 01:41:12 -0500

    added LBTAComponents dependency

---
+++ TwitterClone/HomeDatasourceController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeDatasourceController.swift
+//  TwitterClone
+//
+//  Created by Ashton Morgan on 3/26/17.
+//  Copyright © 2017 Ashton Morgan. All rights reserved.
+//
+
+import Foundation
+import LBTAComponents
+
+class HomeDatasourceController: DatasourceController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView?.backgroundColor = .red
+    }
+    
+}
