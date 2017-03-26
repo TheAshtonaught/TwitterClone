02b1899dfe6b5e23b63017951069878fef057b40
Ashton Morgan <ashtomorgan3@gmail.com>
2017-03-26 03:33:30 -0500

    feat: Implemented basic layout

--- TwitterClone/HomeDatasourceController.swift
+++ TwitterClone/HomeDatasourceController.swift
@@ -13,7 +13,40 @@ class HomeDatasourceController: DatasourceController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView?.backgroundColor = .red
+
+        
+        let homeDatasource = HomeDatasource()
+        self.datasource = homeDatasource
+        
+    
     }
     
+    override func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 150)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        
+        return CGSize(width: view.frame.width, height: 50)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        
+        return CGSize(width: view.frame.width, height: 100)
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
